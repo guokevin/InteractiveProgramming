b21cc9a1790c15e5d3f1f05313195964584dcdb9
Kevin Guo <kevin.guo@students.olin.edu>
2016-03-09 01:04:03 -0500

    Monster Works network

--- server.py
+++ server.py
@@ -1,7 +1,3 @@
-# Excuse me for the my bad English, I hope you can understand my comments
-# pygame site: www.pygame.org
-
-# import pygame
 import pygame
 # import network modules
 from PodSixNet.Channel import Channel
@@ -26,33 +22,92 @@ class GenerateMaze(object):
 class EscapeTheMazeServerModel(object):
     def __init__(self):
         self.players = [] ##keep empty
-        self.number_of_characters = 2   
+        self.NUMBER_OF_CHARACTERS = 2
+        self.NUMBER_OF_SCROLLS = 8
         self.maze = GenerateMaze(10, 10)
         #locations = GenerateCharacterLocations(self)
-        self.char = (Character(550, 550, 20, 20), Character(550, 580, 20, 20))
-        self.still_alive = [True, True]
+        self.char_list = []     ##this list contains a list of attributes for each character (gets sent over network)
+        self.char = []          ##this creates characters for the server
+        self.scroll_list = []
+        self.still_alive = []
+        self.exit = []
+        for i in range(self.NUMBER_OF_CHARACTERS):
+            self.still_alive.append(False)
         self.connected_players = 0
         #locations.char_list
+        GenerateCharacterLocations(self)
+        GenerateScrollLocations(self)
+        GenerateExitLocation(self)
 
-"""
+class GenerateExitLocation(object):
+    def __init__(self,model):
+        self.model = model
+        self.maze = model.maze
+        x = random.randint(0, self.maze.MAZE_LENGTH - 1)
+        x_pos = x*self.maze.MATRIX_CENTERS*2 + self.maze.MATRIX_CENTERS
+        y = random.randint(0, self.maze.MAZE_HEIGHT - 1)
+        y_pos = y*self.maze.MATRIX_CENTERS*2 + self.maze.MATRIX_CENTERS
+        exit_entity = [x_pos, y_pos]
+        self.model.exit = exit_entity
+
+class GenerateScrollLocations(object):
+    def __init__(self, model):
+        self.model = model
+        self.maze = model.maze
+        self.add_scroll = True
+        while len(self.model.scroll_list) < self.model.NUMBER_OF_SCROLLS:
+            x = random.randint(0, self.maze.MAZE_LENGTH - 1)
+            x_pos = x*self.maze.MATRIX_CENTERS*2 + self.maze.MATRIX_CENTERS
+            y = random.randint(0, self.maze.MAZE_HEIGHT - 1)
+            y_pos = y*self.maze.MATRIX_CENTERS*2 + self.maze.MATRIX_CENTERS
+            scroll_entity = [x_pos, y_pos]
+
+            for char in self.model.char_list:
+                if not(x_pos == char[0] and y_pos == char[1]):
+                    if len(self.model.scroll_list) != 0:
+                        for scroll in self.model.scroll_list:
+                                if (x_pos == scroll[0] and y_pos == scroll[1]):
+                                    self.add_scroll = False
+                else:
+                    self.add_scroll = False
+            if self.add_scroll:
+                self.model.scroll_list.append(scroll_entity)
+            self.add_scroll = True
+
+
 class GenerateCharacterLocations(object):
     def __init__(self, model):
         self.model = model
         self.maze = model.maze
-        for i in range(self.model.number_of_characters):
-            x = random.randint(1, self.maze.MAZE_LENGTH - 1)
+        self.add_char = True
+
+        monster_int = random.randint(0,(self.model.NUMBER_OF_CHARACTERS)-1)
+
+        while len(self.model.char_list) < self.model.NUMBER_OF_CHARACTERS:
+            x = random.randint(0, self.maze.MAZE_LENGTH - 1)
             x_pos = x*self.maze.MATRIX_CENTERS*2 + self.maze.MATRIX_CENTERS
-            y = random.randint(1, self.maze.MAZE_HEIGHT - 1)
+            y = random.randint(0, self.maze.MAZE_HEIGHT - 1)
             y_pos = y*self.maze.MATRIX_CENTERS*2 + self.maze.MATRIX_CENTERS
-            char = Character(x_pos, y_pos, 20, 20)
-            self.model.players.append(char)"""
+            #print x,y
+            # print len(self.model.char_list), monster_int
+            
+            char_entity = [x_pos, y_pos, monster_int]
+ 
+            if len(self.model.char_list) != 0:
+                for char in self.model.char_list:
+                    if (x_pos == char[0] and y_pos == char[1]):
+                        self.add_char = False
+            if self.add_char:
+                self.model.char_list.append(char_entity)
+                char = Character(x_pos, y_pos, 20, 20)
+                self.model.char.append(char)
+            self.add_char = True
+
 class Character(object):
     """represents the character"""
     def __init__(self, x_pos, y_pos, width, height):
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.rel_x_pos = x_pos
-        self.rel_y_pos = y_pos
         self.width = width
         self.height = height
         self.rect = pygame.Rect(self.x_pos, self.y_pos, self.width, self.height)
@@ -72,9 +127,10 @@ class ClientChannel(Channel):
         # send to all other clients the information about moving
         self._server.SendToAll(data)
 
-    def Network_alive(self, data):
-        player_num = data['player_number']
-        self.still_alive[player_num] = data['still_alive']
+    def Network_update_alive(self, data):
+        self._server.SendToAll(data)
+
+    def Network_update_entities(self, data):
         self._server.SendToAll(data)
 
 # class representing the server
@@ -90,13 +146,6 @@ class MyServer(Server):
         self.start = False
         # time before strating the game in milliseconds
         self.wait_to_start = -1
-
-        self.monster = random.randint(0,1)
-        self.monster_list = [False, False]
-        for i in range(2):
-            if i == self.monster:
-                self.monster_list[i] = True
-        
         # addresss and port at which server is started
         address, port = kwargs['localaddr']
         
@@ -109,21 +158,26 @@ class MyServer(Server):
         
         # add player to the list
         self.model.players.append(player)
-        # set the bar rect of the player
         self.model.connected_players = len(self.model.players)
-        # print self.model.connected_players
+        # set the bar rect of the player
         player.char = self.model.char[len(self.model.players)-1]      ##add player.char to players[]
-        player.still_alive = self.model.still_alive
+        #player.Char = self.characters[len(self.players)-1]
         # send to the player his number
+        # self.model.
         player.Send({'action': 'number', 'num': len(self.model.players)-1})
-        player.Send({'action': 'monster_number', 'monster_num': self.monster})
-        player.Send({'action': 'is_monster', 'monster': self.monster_list})  
         player.Send({'action': 'generate_maze', 'maze_matrix' : self.model.maze.maze_matrix})
-
+        player.Send({'action': 'initialize_entities', 'char_list' : self.model.char_list, 'scroll_list' : self.model.scroll_list})
+        player.Send({'action': 'exit_location', 'exit': self.model.exit})
+        # player.Send({'action': 'monster_number', 'monster_num': self.monster})
+        # player.Send({'action': 'is_monster', 'monster': self.monster_list})  
+        # if there are two player we can start the game
+        #print len(self.model.players)
+        #print self.model.number_of_characters
         self.SendToAll({'action': 'ready_players', 'connected_players': self.model.connected_players})
-        if len(self.model.players) == 2: #self.model.number_of_characters:
+# 
+        if len(self.model.players) == self.model.NUMBER_OF_CHARACTERS:
             # send to all players the ready message
-            self.SendToAll({'action': 'monster_list'})
+            # self.SendToAll({'action': 'monster_list'})
             self.SendToAll({'action': 'ready'}) 
             # wait 4 seconds before starting the game
             self.wait_to_start = 300
@@ -140,7 +194,6 @@ class MyServer(Server):
             # if the game is started
             # wait 25 milliseconds
             #pygame.time.wait(2)
-            
             # reduce wait to start time if necessary
             if self.wait_to_start > 0:
                 self.wait_to_start -= 25
